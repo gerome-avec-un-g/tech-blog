ea8348275f66e30e9b4d40f10905b0517f0b725d
gerome <gerome.parmentier@gmail.com>
2025-01-07 12:06:15 +0100

    convert javacode to html

--- src/app/java-version/java-version.component.ts
+++ src/app/java-version/java-version.component.ts
@@ -1,30 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, ViewEncapsulation} from '@angular/core';
 import { CommonModule } from '@angular/common'
 import {MatCardModule} from '@angular/material/card';
-import * as jsonData from '../../assets/data.json';
+import * as javaVersionsJson from '../../assets/java-versions.json';
 
 
 @Component({
   selector: 'app-java-version',
   imports: [CommonModule, MatCardModule],
   templateUrl: './java-version.component.html',
-  styleUrl: './java-version.component.css'
+  styleUrl: './java-version.component.css',
+  encapsulation: ViewEncapsulation.None,// required for styling innerHTML
 })
 export class JavaVersionComponent {
-  stream= 'public List<Book> booksWithAtLeastNumberOfPagesStream(List<Book> allBooks, int minimumPageCount) {\n' +
-    '        return allBooks.stream()\n' +
-    '                .filter(book -> book.getPageCount() >= minimumPageCount)\n' +
-    '                .toList();\n' +
-    '    }';
-  forLoop= 'public List<Book> booksWithAtLeastNumberOfPages(List<Book> allBooks, int minimumPageCount) {\n' +
-    '        List<Book> booksWithAtLeastNumberOfPages = new ArrayList<>();\n' +
-    '        for (Book book : allBooks) {\n' +
-    '            if (book.getPageCount() >= minimumPageCount) {\n' +
-    '                booksWithAtLeastNumberOfPages.add(book);\n' +
-    '            }\n' +
-    '        }\n' +
-    '        return booksWithAtLeastNumberOfPages;\n' +
-    '    }';
-  data: any = jsonData;
-  products: any = (jsonData as any).default;
+  javaVersions: any = (javaVersionsJson as any).default;
+
+  toHtml(code: string) {
+    return code
+      .replaceAll("<", "&lt;")
+      .replaceAll(">", "&gt;")
+      .replace(/(?:\r\n|\r|\n)/g, '<br>')
+      .replace(" public ", "&nbsp;<span class='reserved'>public</span>&nbsp;")
+      .replace(" new ", "&nbsp;<span class='reserved'>new</span>&nbsp;")
+      .replace(" for ", "&nbsp;<span class='reserved'>for</span>&nbsp;")
+      .replace(" if ", "&nbsp;<span class='reserved'>if</span>&nbsp;")
+      .replace(" int ", "&nbsp;<span class='reserved'>int</span>&nbsp;")
+      .replace(" return ", "&nbsp;<span class='reserved'>return</span>&nbsp;");
+  }
 }
